0665a30891e169e550ec42474d70efd7d80fef1b
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2024-02-19 14:02:05 -0800

    InitializeProcessHieracrchy() and createChild()

--- main.c
+++ main.c
@@ -1,20 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+//todo use maxProcesses
 #define MAX_Processes 25
 
+typedef struct Child {
+    int id;
+    struct Child* next;
+} Child;
+
+typedef struct PCB{
+    int parentID ;
+    Child* children;
+} PCB;
+PCB* PCB_array[MAX_Processes];
 //skeleton functions TO-Do:
 void initializeProcessHierarchy(){
-    //todo
+    PCB* first = (PCB*)malloc(sizeof(PCB));
+    first ->parentID = -1;
+    first ->children = NULL;
+    PCB_array[0] = first;
+    for (int i =1; i< MAX_Processes; i++){
+        PCB_array[i] = NULL;
+    }
 }
-void createChild(int process_p){
-    //todo
+//function finds an open index where a PCB could go
+int findOpenPCB(){
+    for(int i = 0; i<MAX_Processes;i++){
+        if(!PCB_array[i]){
+            return i;
+        }
+    }
+        return -1;
+}
+void appendChild(Child* curr, int c_id){
+//case 1: if curr is null
+if(!curr){
+    curr = (Child*)malloc(sizeof(Child));
+    curr ->id = c_id;
+}
+//case 2: if curr is not null
+else{
+        appendChild(curr->next,c_id);
+}
+}
+void createChild(int P_ID){
+    //find open ID for new child
+    int  ChildID = findOpenPCB();
+    if(ChildID == -1){ return;}
+    //dynamically allocate a PCB
+    PCB* child = (PCB*)malloc(sizeof(PCB));
+    //add the child to array
+    PCB_array[ChildID] = child;
+    //initialize the child
+    child ->parentID = P_ID;
+    child ->children = NULL;
+    //add this child to the parents children
+    appendChild(PCB_array[P_ID]->children, ChildID);
 };
-void destroy_descendants(int process_p){
-    //todo
+void destroy_descendants(int P_ID){
+    //todo recursively call this method to destroy children
+    //todo free the memory of each child PCB and linked list node
 };
 void printPCB(){
+    //todo
     printf("CHANGE PRINTPCB");
 };
 
@@ -23,20 +72,20 @@ void printPCB(){
 int menu(int option){
     int id;
     switch(option){
-        case 1:
+        case 1://initialize
             initializeProcessHierarchy();
             break;
-        case 2:
+        case 2://create child
             printf("Enter the parent process id: ");
             scanf("%d", &id);
             createChild(id);
             break;
-        case 3:
+        case 3://destroy descendants
             printf("Enter the parent process whose descendants are to be destroyed: ");
             scanf("%d", &id);
             destroy_descendants(id);
             break;
-        case 4:
+        case 4://Quit program and free memory
             printf("Quitting program...");
             //todo free memory
             return 0;
@@ -63,5 +112,4 @@ int main() {
     }
 
 }
-
 }

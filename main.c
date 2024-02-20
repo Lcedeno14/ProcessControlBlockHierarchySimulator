3d6c125fa4de688c2c6335dbd7cc67219f9a7bdb
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2024-02-19 19:25:00 -0800

    finished simulation of pcb blocks and free memory

--- main.c
+++ main.c
@@ -49,6 +49,12 @@ void appendChild(Child** head, int c_id){
         temp->next = newChild;
     }
 }
+bool invalid_id(int id){
+    if (id < 0 || id >= MAX_Processes || PCB_array[id] == NULL) {
+        return true;
+    }
+    return false;
+}
 // Modify the createChild function call to appendChild accordingly.
 
 void createChild(int P_ID){
@@ -66,8 +72,25 @@ void createChild(int P_ID){
     appendChild(&(PCB_array[P_ID]->children), ChildID);
 };
 void destroy_descendants(int P_ID){
-    //todo recursively call this method to destroy children
-    //todo free the memory of each child PCB and linked list node
+   //set up
+    Child* child = PCB_array[P_ID]->children;
+    Child* temp;
+    //recursive loop to destroy children
+    while (child != NULL) {
+        int childID = child->id; // Get the ID of the child
+        //recursively destroy the descendants
+        if (!invalid_id(childID)) {
+            destroy_descendants(childID);
+            //free the PCB of the child
+            free(PCB_array[childID]);
+            PCB_array[childID] = NULL; //
+        }
+
+        temp = child; // Hold the current child to free it after moving to the next
+        child = child->next; // Move to the next child
+        free(temp); // Free the current child node
+    }
+    PCB_array[P_ID]->children = NULL;
 };
 void printPCB(){
    printf("Process list:\n");
@@ -99,21 +122,27 @@ int menu(int option){
     switch(option){
         case 1://initialize
             initializeProcessHierarchy();
+            printf("\n");
             break;
         case 2://create child
-            printf("\nEnter the parent process id: ");
+            printf("Enter the parent process id: ");
             scanf("%d", &id);
-            //todo fix case where user enters an id that doesn't exist or hasn't been initialized
+            printf("\n");
+            if(invalid_id(id)){break;}
             createChild(id);
             break;
         case 3://destroy descendants
             printf("Enter the parent process whose descendants are to be destroyed: ");
             scanf("%d", &id);
+            printf("\n");
+            if(invalid_id(id)){break;}
             destroy_descendants(id);
             break;
         case 4://Quit program and free memory
+            printf("\n");
             printf("Quitting program...");
-            //todo free memory
+            destroy_descendants(0);
+            free(PCB_array[0]);
             return 0;
         default:
             return 1;
@@ -132,8 +161,7 @@ int main() {
     printf("3) Destroy all descendants of a process\n");
     printf("4) Quit program and free memory\n\n");
     printf("Enter selection: ");
-    scanf("%d", &option);
-    //printf("\n");
+    scanf(" %d", &option);
     if(!menu(option)){
         return 0;
     }
